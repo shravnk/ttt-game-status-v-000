8722a607c7f3c21cd7b4db6dadf93bb3e2658aa3
Brendan O'Dea <brendan.ode4@gmail.com>
2017-12-26 16:59:58 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,16 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  
+]
+
 # Define your WIN_COMBINATIONS constant
